1694e81b61f2bd1cc6aed5ef5b946413c1b216b4
duracik2 <duracik2@RA323-00.fri.uniza.sk>
2018-03-27 14:46:32 +0200

    Cvicenie 27.3.2018 - Triedy pre ReplikaHraca a ReplikaNPC

---
+++ src/sk/uniza/fri/wof/herneProstredie/dialogy/ReplikaHraca.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.uniza.fri.wof.herneProstredie.dialogy;
+
+import sk.uniza.fri.wof.herneProstredie.Hrac;
+
+
+/**
+ *
+ * @author duracik2
+ */
+public class ReplikaHraca {
+    
+    
+    public String dajSpravu() {
+        
+    }
+    
+    public ReplikaNPC dalsiaReplikaNPC() {
+        
+    }
+    
+    public void vykonaj(Hrac hrac) {
+        
+    }
+}
---
+++ src/sk/uniza/fri/wof/herneProstredie/dialogy/ReplikaNPC.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.uniza.fri.wof.herneProstredie.dialogy;
+
+/**
+ *
+ * @author duracik2
+ */
+public class ReplikaNPC {
+    
+    public String dajSpravu() {
+        
+    }
+    
+    public void vypisMoznosti() {
+        
+    }
+    
+    public ReplikaHraca vyberReplikuHraca(int cisloRepliky) {
+        
+    }
+}
