2ce7ee52ac491caeff5312f0fc867eae8db07b77
Unlink <miso250593@gmail.com>
2018-04-24 09:52:00 +0200

    Nacitavanie dveri

--- src/sk/uniza/fri/wof/mapa/NacitavacMapy.java
+++ src/sk/uniza/fri/wof/mapa/NacitavacMapy.java
@@ -14,8 +14,13 @@ import java.util.Scanner;
 import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import sk.uniza.fri.wof.herneProstredie.DvereNaKluc;
+import sk.uniza.fri.wof.herneProstredie.IDvere;
 import sk.uniza.fri.wof.herneProstredie.Miestnost;
+import sk.uniza.fri.wof.herneProstredie.ObycajneDvere;
 import sk.uniza.fri.wof.herneProstredie.PortalGun;
+import sk.uniza.fri.wof.herneProstredie.Vytah;
+import sk.uniza.fri.wof.herneProstredie.VytahoveDvere;
 import sk.uniza.fri.wof.herneProstredie.predmety.Hodinky;
 import sk.uniza.fri.wof.herneProstredie.predmety.IPredmet;
 import sk.uniza.fri.wof.herneProstredie.predmety.Kluc;
@@ -41,6 +46,7 @@ public class NacitavacMapy {
         try {
             Uzol koren = fileReader.nahrajSubor(cestaKSuboru);
             this.nacitajMiestnosti(koren.najdiPotomka("miestnosti"));
+            this.nacitajDvere(koren.najdiPotomka("dvere"));
             return this.miestnosti.get(koren.najdiPotomka("start").getHodnota());
         } catch (IOException ex) {
             
@@ -90,6 +96,43 @@ public class NacitavacMapy {
                 return new PortalGun();
              case "Pacidlo":
                 return new Pacidlo();
+            default:
+                return null;
+        }
+    }
+
+    private void nacitajDvere(Uzol dvere) {
+        for (Uzol info : dvere.getPotomkovia()) {
+            //Obycajne dvere
+            if (info.isVirtual()) {
+                String[] nazovMiestnosti = info.getHodnota().split(" ");
+                IDvere d = new ObycajneDvere(this.miestnosti.get(nazovMiestnosti[0]), this.miestnosti.get(nazovMiestnosti[1]));
+            }
+            else {
+                Scanner scanner = new Scanner(info.getHodnota());
+                ArrayList<String> parametre = new ArrayList<>();
+                while (scanner.hasNext()) {
+                    parametre.add(scanner.next());
+                }
+                vyrvorDvere(info.getKluc(), parametre);
+            }
+        }
+    }
+
+    private IDvere vyrvorDvere(String className, ArrayList<String> parametre) {
+        switch (className) {
+            case "DvereNaKluc":
+                DvereNaKluc dvere = new DvereNaKluc(this.miestnosti.get(parametre.get(0)), this.miestnosti.get(parametre.get(1)));
+                for (int i = 2; i < parametre.size(); i++) {
+                    Kluc kluc = (Kluc) this.predmety.get(parametre.get(i));
+                    kluc.pridajDvere(dvere);
+                }
+                return dvere;
+            case "VytahoveDvere":
+                Miestnost m1 = this.miestnosti.get(parametre.get(0));
+                Miestnost m2 = this.miestnosti.get(parametre.get(1));
+                int poschodie = Integer.parseInt(parametre.get(2));
+                return new VytahoveDvere((Vytah) m2, m1, poschodie);
             default:
                 return null;
         }
